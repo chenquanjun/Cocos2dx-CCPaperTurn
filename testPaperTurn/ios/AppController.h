7b3b9b4ff3baa67f0ab66eababd79ad66a781b9e
chenquanjun <chenquanjun@chenquanjuntekiiMac.local>
2014-01-24 16:48:03 +0800

    Initial Commit

---
+++ testPaperTurn/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  testPaperTurnAppController.h
+//  testPaperTurn
+//
+//  Created by chenquanjun on 14-1-24.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ testPaperTurn/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  testPaperTurnAppController.h
+//  testPaperTurn
+//
+//  Created by chenquanjun on 14-1-24.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
