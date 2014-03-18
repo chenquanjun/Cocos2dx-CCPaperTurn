8b7738cd7b25411237063bda614c44b176e1d657
chenquanjun <chenshiyouxiang@gmail.com>
2014-03-18 15:36:13 +0800

    first commit

---
+++ testPaperTurn/CCPaperTurn.cpp
@@ -0,0 +1,9 @@
+//
+//  CCPaperTurn.cpp
+//  testPaperTurn
+//
+//  Created by chenquanjun on 14-1-24.
+//
+//
+
+#include "CCPaperTurn.h"
---
+++ testPaperTurn/CCPaperTurn.h
@@ -0,0 +1,14 @@
+//
+//  CCPaperTurn.h
+//  testPaperTurn
+//
+//  Created by chenquanjun on 14-1-24.
+//
+//
+
+#ifndef __testPaperTurn__CCPaperTurn__
+#define __testPaperTurn__CCPaperTurn__
+
+#include <iostream>
+
+#endif /* defined(__testPaperTurn__CCPaperTurn__) */
